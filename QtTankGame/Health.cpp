de6c6f6e9b2daeaa6a48da1cabc59528a2831457
srodriguez99 <54964625+srodriguez99@users.noreply.github.com>
2021-05-01 17:46:54 -0700

    Created Health

---
+++ QtTankGame/Health.cpp
@@ -0,0 +1,29 @@
+#include "Health.h"
+#include <QFont>
+
+Health::Health(QGraphicsItem *parent): QGraphicsTextItem(parent)
+{
+    healthT1 = 100;
+    healthT2 = 100;
+
+    setPlainText(QString("Tank1 Health: ") + QString::number(healthT1));
+    setDefaultTextColor(Qt::blue);
+    setFont(QFont("times",16));
+
+    setPlainText(QString("Tank2 Health: ") + QString::number(healthT2));
+
+}
+
+//if any health was
+void Health::decrease(int t1, int t2)
+{
+    healthT1 = healthT1 - t1;
+    healthT2 = healthT2 - t2;
+}
+
+
+
+int Health::getHealth()
+{
+    return
+}
---
+++ QtTankGame/Health.h
@@ -0,0 +1,23 @@
+#ifndef HEALTH_H
+#define HEALTH_H
+
+#include <QGraphicsSimpleTextItem>
+
+class Health: public QGraphicsTextItem{
+public:
+    Health(QGraphicsItem * parent = 0);
+    //update health for player
+    //take inputs of how much damage was taken if any
+    void decrease(int t1, int t2);
+
+    //get health for player
+    int getHealth();
+
+private:
+    int healthT1;
+    int healthT2;
+
+
+};
+
+#endif // HEALTH_H
