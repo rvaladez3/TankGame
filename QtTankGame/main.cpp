eeb5aa71379e75d52573de4a549ea8dfa9a54368
srodriguez99 <54964625+srodriguez99@users.noreply.github.com>
2021-05-04 15:33:52 -0700

    New Scene

--- QtTankGame/main.cpp
+++ QtTankGame/main.cpp
@@ -5,19 +5,29 @@
 #include <QDebug>
 #include <QGraphicsScene>
 #include <QApplication>
-#include <QGraphicsRectItem>
+#include "mainwindow.h"
 #include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QGraphicsScene * scene = new QGraphicsScene();
-    QGraphicsRectItem * rect = new QGraphicsRectItem;
+    MainWindow * rect = new MainWindow();
+
+
     rect->setRect(0,0,100,100);
 
     scene->addItem(rect);
 
+    //make item focusable
+    rect->setFlag(QGraphicsItem::ItemIsFocusable);
+    rect->setFocus();
+
+
     QGraphicsView * view = new QGraphicsView(scene);
+    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
     view->show();
     return a.exec();
 }
--- QtTankGame/mainwindow.cpp
+++ QtTankGame/mainwindow.cpp
@@ -6,107 +6,139 @@
 #include <QGraphicsScene>
 
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-}
-
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
-
+//new code
 void MainWindow::keyPressEvent(QKeyEvent *event)
 {
-        float y_inc = 0.0;
-        float x_inc = 0.0;
+    if (event->key() == Qt::Key_Up) {
+       setPos(x(),y()-10);
+    }
 
-        float y1 = ui->label->y();
-        float x1 = ui->label->x();
+    if (event->key() == Qt::Key_Down) {
+       setPos(x(),y()+10);
+    }
 
-        float y2 = ui->label_2->y();
-        float x2 = ui->label_2->x();
+    if (event->key() == Qt::Key_Left) {
+       setPos(x()-10,y());
+    }
 
-        bool player = false;
+    if (event->key() == Qt::Key_Right) {
+       setPos(x()+10,y());
+    }
 
-
-
-        //label_2
-        if (event->key() == Qt::Key_Up && y2 > 10) {
-            y_inc = -5.0f;
-            player = true;
-        }
-
-        if (event->key() == Qt::Key_Down && y2 < 530) {
-            y_inc = +5.0f;
-            player = true;
-        }
-        //can't move past mid point
-        if (event->key() == Qt::Key_Left && x2 > 530) {
-            x_inc = -5.0f;
-            player = true;
-        }
-
-        if (event->key() == Qt::Key_Right && x2 < 1000) {
-            x_inc = +5.0f;
-            player = true;
-        }
-
-        if(event->key() == Qt::Key_Space){
-            Projectile * projectile = new Projectile();
-            qDebug() <<"Projectile created";
-            projectile->setPos(ui->label_2->x(),ui->label_2->y());
-            scene->addItem(projectile);
-        }
-
-        ui->label_2->move(QPoint(x2 + x_inc, y2 + y_inc));
-
-        if(player == false)
-        {
-        //label
-        if (event->key() == Qt::Key_W && y1 > 10) {
-            y_inc = -5.0f;
-        }
-
-        if (event->key() == Qt::Key_S && y1 < 530) {
-            y_inc = +5.0f;
-        }
-
-        if (event->key() == Qt::Key_A && x1 > 10) {
-            x_inc = -5.0f;
-        }
-        //cant move past mid point
-        if (event->key() == Qt::Key_D && x1 < 470) {
-            x_inc = +5.0f;
-        }
-
-            ui->label->move(QPoint(x1 + x_inc, y1 + y_inc));
-        }
-}
-
-void MainWindow::mousePressEvent(QMouseEvent *event)
-{
-    lastX = event->x();
-    lastY = event->y();
-}
-
-void MainWindow::mouseMoveEvent(QMouseEvent *event)
-{
-    float xAtRelease = event->x();
-    float yAtRelease = event->y();
-
-    float dx = xAtRelease - lastX;
-    float dy = yAtRelease - lastY;
-
-    float x = ui->label->x();
-    float y = ui->label->y();
-
-    ui->label->move(QPoint(x + dx, y + dy));
-
-    lastX = event->x();
-    lastY = event->y();
+    if(event->key() == Qt::Key_Space){
+        Projectile * projectile = new Projectile();
+        qDebug() <<"Projectile created";
+        projectile->setPos(x(),y());
+        scene->addItem(projectile);
+    }
 
 }
+
+
+
+//MainWindow::MainWindow(QWidget *parent)
+//    : QMainWindow(parent)
+//    , ui(new Ui::MainWindow)
+//{
+//    ui->setupUi(this);
+//}
+
+//MainWindow::~MainWindow()
+//{
+//    delete ui;
+//}
+
+//void MainWindow::keyPressEvent(QKeyEvent *event)
+//{
+//        float y_inc = 0.0;
+//        float x_inc = 0.0;
+
+//        float y1 = ui->label->y();
+//        float x1 = ui->label->x();
+
+//        float y2 = ui->label_2->y();
+//        float x2 = ui->label_2->x();
+
+//        bool player = false;
+
+
+
+//        //label_2
+//        if (event->key() == Qt::Key_Up && y2 > 10) {
+//            y_inc = -5.0f;
+//            player = true;
+//        }
+
+//        if (event->key() == Qt::Key_Down && y2 < 530) {
+//            y_inc = +5.0f;
+//            player = true;
+//        }
+//        //can't move past mid point
+//        if (event->key() == Qt::Key_Left && x2 > 530) {
+//            x_inc = -5.0f;
+//            player = true;
+//        }
+
+//        if (event->key() == Qt::Key_Right && x2 < 1000) {
+//            x_inc = +5.0f;
+//            player = true;
+//        }
+
+//        if(event->key() == Qt::Key_Space){
+//            Projectile * projectile = new Projectile();
+//            qDebug() <<"Projectile created";
+//            projectile->setPos(ui->label_2->x(),ui->label_2->y());
+//            //scene->addItem(projectile);
+//        }
+
+//        ui->label_2->move(QPoint(x2 + x_inc, y2 + y_inc));
+
+//        if(player == false)
+//        {
+//        //label
+//        if (event->key() == Qt::Key_W && y1 > 10) {
+//            y_inc = -5.0f;
+//        }
+
+//        if (event->key() == Qt::Key_S && y1 < 530) {
+//            y_inc = +5.0f;
+//        }
+
+//        if (event->key() == Qt::Key_A && x1 > 10) {
+//            x_inc = -5.0f;
+//        }
+//        //cant move past mid point
+//        if (event->key() == Qt::Key_D && x1 < 470) {
+//            x_inc = +5.0f;
+//        }
+
+//            ui->label->move(QPoint(x1 + x_inc, y1 + y_inc));
+//        }
+//}
+
+////void MainWindow::mousePressEvent(QMouseEvent *event)
+////{
+////    lastX = event->x();
+////    lastY = event->y();
+////}
+
+////void MainWindow::mouseMoveEvent(QMouseEvent *event)
+////{
+////    float xAtRelease = event->x();
+////    float yAtRelease = event->y();
+
+////    float dx = xAtRelease - lastX;
+////    float dy = yAtRelease - lastY;
+
+////    float x = ui->label->x();
+////    float y = ui->label->y();
+
+////    ui->label->move(QPoint(x + dx, y + dy));
+
+////    lastX = event->x();
+////    lastY = event->y();
+
+////}
+
+
 
